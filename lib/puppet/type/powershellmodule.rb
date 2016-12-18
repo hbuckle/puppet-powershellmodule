c7cabdb2f3f63b935c9010db80a5ad0d91952aee
Henry Buckle <h.buckle@outlook.com>
2016-12-18 12:50:14 +0000

    Rubocop

--- lib/puppet/type/powershellmodule.rb
+++ lib/puppet/type/powershellmodule.rb
@@ -1,20 +1,18 @@
 Puppet::Type.newtype(:powershellmodule) do
-  @doc = "Manage Windows PowerShell modules"
+  @doc = 'Manage PowerShell modules'
 
   ensurable
 
   newparam(:name) do
-    desc "The name of the module"
-    munge do |value|
-      value.downcase
-    end
+    desc 'The name of the module'
+    munge(&:downcase)
   end
 
   newproperty(:version) do
-    desc "The version to install"
+    desc 'The version to install'
   end
 
   newparam(:repository) do
-    desc "The repository to install from. Must be registered on the system"
+    desc 'The repository to install from. Must be registered on the system'
   end
 end
--- lib/puppet/type/psrepository.rb
+++ lib/puppet/type/psrepository.rb
@@ -1,27 +1,25 @@
 Puppet::Type.newtype(:psrepository) do
-  @doc = "Manage sources for PowerShell modules"
+  @doc = 'Manage sources for PowerShell modules'
 
   ensurable
 
   newparam(:name) do
-    desc "The name of the repository"
+    desc 'The name of the repository'
   end
 
   newproperty(:source_location) do
-    desc "The source location"
+    desc 'The source location'
     validate do |value|
       unless URI.parse(value).is_a?(URI::HTTP) ||
-              URI.parse(value).is_a?(URI::HTTPS)
-        raise Puppet::ResourceError, "%s is not a valid URI" % value
+             URI.parse(value).is_a?(URI::HTTPS)
+        raise Puppet::ResourceError "#{value} is not a valid URI"
       end
     end
-    munge do |value|
-      value.downcase
-    end
+    munge(&:downcase)
   end
 
   newproperty(:installation_policy) do
-    desc "Installation policy for this repository. Must be trusted or untrusted"
+    desc 'Installation policy for this repository. Must be trusted or untrusted'
     defaultto :untrusted
     newvalues(:trusted, :untrusted)
   end
