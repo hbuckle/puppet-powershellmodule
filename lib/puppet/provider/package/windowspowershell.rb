ea496c503c0b211e7b11588f7a8c2769ccd14336
Paul Broadwith <pauby@users.noreply.github.com>
2020-04-25 13:19:14 +0100

    Add TLS 1.2 support for Windows PowerShell

--- lib/puppet/provider/package/windowspowershell.rb
+++ lib/puppet/provider/package/windowspowershell.rb
@@ -7,7 +7,7 @@ Puppet::Type.type(:package).provide(:windowspowershell, parent: :powershellcore)
 
   def self.invoke_ps_command(command)
     result = powershell(['-NoProfile', '-ExecutionPolicy', 'Bypass', '-NonInteractive', '-NoLogo', '-Command',
-                         "$ProgressPreference = 'SilentlyContinue'; $ErrorActionPreference = 'Stop'; #{command}"])
+                         "$ProgressPreference = 'SilentlyContinue'; $ErrorActionPreference = 'Stop'; [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; #{command}"])
     result.lines
   end
 end
--- lib/puppet/provider/pspackageprovider/windowspowershell.rb
+++ lib/puppet/provider/pspackageprovider/windowspowershell.rb
@@ -4,7 +4,7 @@ Puppet::Type.type(:pspackageprovider).provide(:windowspowershell, parent: :power
 
   def self.invoke_ps_command(command)
     result = powershell(['-NoProfile', '-ExecutionPolicy', 'Bypass', '-NonInteractive', '-NoLogo', '-Command',
-                         "$ProgressPreference = 'SilentlyContinue'; $ErrorActionPreference = 'Stop'; #{command}"])
+                         "$ProgressPreference = 'SilentlyContinue'; $ErrorActionPreference = 'Stop'; [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; #{command}"])
     result.lines
   end
 end
