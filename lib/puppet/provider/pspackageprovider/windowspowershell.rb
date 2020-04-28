40abc09e75d69e58e2f145ecbc6d5070a71a426e
Greg Perry <gsperry2011@gmail.com>
2020-04-28 11:17:30 -0400

    Comment addition

--- lib/puppet/provider/pspackageprovider/windowspowershell.rb
+++ lib/puppet/provider/pspackageprovider/windowspowershell.rb
@@ -4,6 +4,8 @@ Puppet::Type.type(:pspackageprovider).provide(:windowspowershell, parent: :power
 
   def self.invoke_ps_command(command)
     result = powershell(['-NoProfile', '-ExecutionPolicy', 'Bypass', '-NonInteractive', '-NoLogo', '-Command',
+                         # The following section of the -Command forces powershell to use tls1.2 (which it does not by default currently unless set system wide): [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+                         # Without tls1.2 you cannot install modules from PSGallery
                          "$ProgressPreference = 'SilentlyContinue'; $ErrorActionPreference = 'Stop'; [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; #{command}"])
     result.lines
   end
